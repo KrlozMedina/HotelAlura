afbb7570f274036a977a50fa13e0d90e4b94d509
KrlozMedina <charli1216@hotmail.com>
2023-04-29 12:52:33 -0500

    [CONVERT] To IntelliJ

--- src/views/MenuPrincipal.java
+++ src/views/MenuPrincipal.java
@@ -1,17 +1,8 @@
 package views;
 
-import java.awt.EventQueue;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import java.awt.Panel;
-import java.awt.Color;
-import java.awt.SystemColor;
-import javax.swing.JLabel;
-import javax.swing.ImageIcon;
-import java.awt.Font;
-import java.awt.Toolkit;
-import javax.swing.SwingConstants;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
